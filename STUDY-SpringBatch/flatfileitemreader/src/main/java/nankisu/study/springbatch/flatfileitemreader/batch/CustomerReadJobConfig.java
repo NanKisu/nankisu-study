77a879301b0b34a973005e2042e97eba9b248c1d
nankisu0301 <nankisu0301@naver.com>
2022-03-15 21:46:32 +0900

    study: spring-batch DefaultLineMapper

--- STUDY-SpringBatch/flatfileitemreader/src/main/java/nankisu/study/springbatch/flatfileitemreader/batch/CustomerReadJobConfig.java
+++ STUDY-SpringBatch/flatfileitemreader/src/main/java/nankisu/study/springbatch/flatfileitemreader/batch/CustomerReadJobConfig.java
@@ -5,10 +5,13 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemStreamReader;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -26,10 +29,17 @@ public class CustomerReadJobConfig {
 	private final CustomerProcessor customerProcessor;
 	private final CustomerWriter customerWriter;
 
-	@Bean
 	public Job customerReadJob() {
 		return jobBuilderFactory.get("customerReadJob")
 				.start(customerReadStep())
+				.build();
+	}
+	
+	@Bean
+	public Job customerReadJob2() {
+		return jobBuilderFactory.get("customerReadJob2")
+				.start(customerReadStep())
+				.incrementer(new RunIdIncrementer())
 				.build();
 	}
 	
@@ -44,11 +54,32 @@ public class CustomerReadJobConfig {
 	}
 	
 	@Bean
-	public ItemStreamReader<CustomerVo> customerFlatFileItemReader() {
+	public Step customerReadStep2() {
+		return stepBuilderFactory.get("customerReadStep2")
+				.<CustomerVo, String>chunk(5)
+				.reader(customerFlatFileItemReader2())
+				.processor(customerProcessor)
+				.writer(customerWriter)
+				.build();
+	}
+	
+	@Bean
+	public ItemReader<CustomerVo> customerFlatFileItemReader() {
 		return new FlatFileItemReaderBuilder<CustomerVo>().name("customerFlatFileItemReader")
 				.resource(new ClassPathResource("/customers.csv"))
 				.linesToSkip(1)
 				.lineMapper(new CustomerLineMapper(new DelimitedLineTokenizer(), new CustomerFieldSetMapper()))
+				.build();
+	}
+
+	@Bean
+	public ItemReader<CustomerVo> customerFlatFileItemReader2() {
+		return new FlatFileItemReaderBuilder<CustomerVo>().name("customerFlatFileItemReader2")
+				.resource(new ClassPathResource("/customers.csv"))
+				.linesToSkip(1)
+				.delimited().delimiter(",")
+				.names("name", "age")
+				.fieldSetMapper(new BeanWrapperFieldSetMapper<CustomerVo>())
 				.build();
 	}
 }
