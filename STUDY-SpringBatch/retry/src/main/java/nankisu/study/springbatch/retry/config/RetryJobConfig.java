3466428a3edd7f7224638d45ab9d663f29d8c8c8
nankisu0301 <nankisu0301@naver.com>
2022-07-28 01:49:25 +0900

    study: spring-batch backoffPolicy, retryTemplate

--- STUDY-SpringBatch/retry/src/main/java/nankisu/study/springbatch/retry/config/RetryJobConfig.java
+++ STUDY-SpringBatch/retry/src/main/java/nankisu/study/springbatch/retry/config/RetryJobConfig.java
@@ -1,6 +1,7 @@
 package nankisu.study.springbatch.retry.config;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
@@ -10,8 +11,16 @@ import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.batch.repeat.support.RepeatTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.RecoveryCallback;
+import org.springframework.retry.RetryCallback;
+import org.springframework.retry.RetryContext;
+import org.springframework.retry.backoff.BackOffPolicy;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 
 import lombok.RequiredArgsConstructor;
 
@@ -35,14 +44,39 @@ public class RetryJobConfig {
 				.<Integer, String>chunk(5)
 				.reader(new ListItemReader<Integer>(List.of(1,2,3,4,5,6,7,8,9,10)))
 				.processor(new ItemProcessor<Integer, String>() {
+					private RetryTemplate retryTemplate;
+					
 					@Override
 					public String process(Integer item) throws Exception {
-						// TODO Auto-generated method stub
-						System.out.println(item);
 //						if(item == 2) {
 //							throw new Exception();
 //						}
-						return item.toString();
+						retryTemplate = new RetryTemplate();
+						SimpleRetryPolicy simpleRetryPolicy = new SimpleRetryPolicy(2, Map.of(Exception.class, true));
+						retryTemplate.setRetryPolicy(simpleRetryPolicy);
+						
+						String result = retryTemplate.execute(
+							new RetryCallback<String, Exception>() {
+								@Override
+								public String doWithRetry(RetryContext context) throws Exception {
+									System.out.println(item);
+									if(item == 2) {
+										System.out.println("throw");
+										throw new Exception();
+									}
+									return item.toString();
+								}
+							},
+							new RecoveryCallback<String>() {
+
+								@Override
+								public String recover(RetryContext context) throws Exception {
+									// TODO Auto-generated method stub
+									return "0";
+								}
+							}
+						);
+						return result;
 					}
 				})
 				.writer(new ItemWriter<String>() {
@@ -60,6 +94,14 @@ public class RetryJobConfig {
 				.skipLimit(2)
 				.retry(Exception.class)
 				.retryLimit(2)
+				.backOffPolicy(backOffPolicy())
 				.build();
+	}
+	
+	@Bean
+	public BackOffPolicy backOffPolicy() {
+		FixedBackOffPolicy fixedBackOffPolicy = new FixedBackOffPolicy();
+		fixedBackOffPolicy.setBackOffPeriod(10000);
+		return fixedBackOffPolicy;
 	}
 }
