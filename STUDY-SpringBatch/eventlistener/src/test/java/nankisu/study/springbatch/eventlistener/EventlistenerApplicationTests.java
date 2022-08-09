ee1ede2d321f29c971e1df4cdb5202d0862a5f01
nankisu0301 <nankisu0301@naver.com>
2022-08-09 21:27:02 +0900

    study: spring-batch batch test

--- STUDY-SpringBatch/eventlistener/src/test/java/nankisu/study/springbatch/eventlistener/EventlistenerApplicationTests.java
+++ STUDY-SpringBatch/eventlistener/src/test/java/nankisu/study/springbatch/eventlistener/EventlistenerApplicationTests.java
@@ -1,13 +1,32 @@
 package nankisu.study.springbatch.eventlistener;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDate;
+
+
 import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class EventlistenerApplicationTests {
+import nankisu.study.springbatch.eventlistener.batch.job.MyJobConfig;
 
+
+@SpringBatchTest
+@SpringBootTest(classes = {MyJobConfig.class, TestBatchConfig.class})
+class EventlistenerApplicationTests {
+	@Autowired
+	private JobLauncherTestUtils jobLauncherTestUtils;
+	
 	@Test
-	void contextLoads() {
+	void jobLauchTest() throws Exception {
+		JobExecution jobExecution = jobLauncherTestUtils.launchJob(new JobParametersBuilder().addLong("launchDate", LocalDate.now().toEpochDay()).toJobParameters());
+		assertThat(jobExecution.getExitStatus().getExitCode()).isEqualTo(ExitStatus.FAILED.getExitCode());
 	}
 
 }
---
+++ STUDY-SpringBatch/eventlistener/src/test/java/nankisu/study/springbatch/eventlistener/TestBatchConfig.java
@@ -0,0 +1,12 @@
+package nankisu.study.springbatch.eventlistener;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableBatchProcessing
+public class TestBatchConfig {
+
+}
